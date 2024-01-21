5dc316c33b00f7487c75057f8310d60d379fe72c
Pierre Lovergne <pierre.lovergne@gmail.com>
2024-01-21 21:55:02 +0100

    page 56

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -7,17 +7,31 @@ import (
 	"github.com/rem0ld/monkey-programming/lexer"
 )
 
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
+}
+
 func TestLetStatement(t *testing.T) {
 	input := `
   let x = 5;
   let y = 10;
-  let foobar = 838383;
+  let 838383;
   `
 
 	l := lexer.New(input)
 	p := New(l)
 
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
